2ac8400fe5be0ecfc36919a1412a27e207b8144a
Danilo <danilmunoz@inf.udec.cl>
2023-05-21 15:28:06 -0400

    refactor data.json

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,7 @@ specs_data = '{{ cookiecutter.specsData }}'
 testimonials_data = '{{ cookiecutter.testimonialsData }}'
 
 # Convirtiendo las cadenas a objetos de Python
+print('specsData: ', specs_data)
 specs_data_obj = json.loads(specs_data)
 testimonials_data_obj = json.loads(testimonials_data)
 
