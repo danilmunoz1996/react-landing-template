f95944072259818ae86569cceda3f339b897466f
Danilo <danilmunoz@inf.udec.cl>
2023-05-25 23:55:23 -0400

    reference images

--- {{cookiecutter.project_name}}/src/components/Sections/NewsletterSubscription.tsx
+++ {{cookiecutter.project_name}}/src/components/Sections/NewsletterSubscription.tsx
@@ -1,33 +1,13 @@
-import React, { ChangeEvent, FormEvent } from 'react';
+import React from 'react';
 
 const NewsletterSubscription: React.FC = () => {
-
-  const [email, setEmail] = React.useState('');
-  const [phone, setPhone] = React.useState('');
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    console.log('event.target.value: ' + event.target.value);
-    setEmail(event.target.value);
-  };
-
-  const handlePhoneChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    console.log('event.target.value: ' + event.target.value);
-    setPhone(event.target.value);
-  };
-
-  const handleSubmit = (event: FormEvent): void => {
-    event.preventDefault();
-    // Aquí podrías manejar la lógica de suscripción
-    console.log('Email: ' + email);
-    console.log('Phone: ' + phone);
-  };
 
   return (
     <div className="container-fluid bg-light py-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
           <h2 className="text-center mb-4">Solicita Atención y nos pondremos en contacto</h2>
-          <form onSubmit={handleSubmit}>
+          <form>
             <div className="input-group mb-3">
               <input
                 type="email"
@@ -35,8 +15,7 @@ const NewsletterSubscription: React.FC = () => {
                 placeholder="Email"
                 aria-label="Email"
                 aria-describedby="button-addon2"
-                value={email}
-                onChange={handleChange}
+                value=""
                 required
               />
             </div>
@@ -47,8 +26,7 @@ const NewsletterSubscription: React.FC = () => {
                 placeholder="Numero de Telefono o WhatsApp"
                 aria-label="Numero de Telefono o WhatsApp"
                 aria-describedby="button-addon2"
-                value={phone}
-                onChange={handlePhoneChange}
+                value=""
                 required
               />
               <button className="btn btn-primary" type="submit" id="button-addon2">
